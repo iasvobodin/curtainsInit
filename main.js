7ba4bd2c5cab65e2281fc3e7e5d3a1ccdddd7432
iasvobodin <iasvobodin@gmail.com>
2021-10-18 16:22:30 +0500

    11

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 import './style.css'
+import {Curtains, Plane} from "curtainsjs";
+import fragment from './dist/init.frag?raw'
+import vertex from './dist/init.vert?raw'
+
+// wait for everything to be ready
+window.addEventListener("load", () => {
+// set up our WebGL context and append the canvas to our wrapper
+const curtains = new Curtains({
+container: "canvas"
+});
+// get our plane element
+const planeElement = document.getElementsByClassName("plane")[0];
+// set our initial parameters (basic uniforms)
+const params = {
+vertexShader: vertex, // our vertex shader ID
+fragmentShader: fragment, // our fragment shader ID
+uniforms: {
+time: {
+name: "uTime", // uniform name that will be passed to our shaders
+type: "1f", // this means our uniform is a float
+value: 0,
+},
+},
+};
+// create our plane using our curtains object, the HTML element and the parameters
+const plane = new Plane(curtains, planeElement, params);
+plane.onRender(() => {
+// use the onRender method of our plane fired at each requestAnimationFrame call
+plane.uniforms.time.value++; // update our time uniform value
+});
+});
